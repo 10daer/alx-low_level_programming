f41295f86a04b3cbdac5d657d0bdb218b24eca4c
10daer <readwanafolabi@gmail.com>
2023-02-18 12:56:17 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - print a line of code with puts
+ *
+ *  return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+		return (0);
+}
