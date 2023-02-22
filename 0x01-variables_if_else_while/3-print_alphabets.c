8cf163026ab898af73ae498bc8f5c8b24b9ded5c
10daer <readwanafolabi@gmail.com>
2023-02-22 05:42:57 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+	char a = 'a';
+	char z = 'z';
+	char A = 'A';
+	char Z = 'Z';
+
+	for (i = a; i <= z; i++)
+	{putchar(i);
+	}
+	for (i = A; i <= Z; i++)
+	{putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
