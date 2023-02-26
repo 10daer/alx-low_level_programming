d05cd5a33c65b4f0af2d88e4be04a18888592569
10daer <afolabireedwan@gmail.com>
2023-02-26 13:16:47 +0100

    task 11

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,20 +7,21 @@
  */
 int main(void)
 {
-		int i;
+		int i, j;
 
-		for (i = 0; i <= 89; i++)
+		for (i = 0; i <= 8; i++)
 		{
-			if (i <= 9)
+			for (j = i + 1; j <= 9; j++)
 			{
 				putchar('0' + i);
-			} else
-			{
-				putchar('0' + i / 10);
-				putchar('0' + i % 10);
+				putchar('0' + j);
+				if (i < 8 || j < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			putchar(',');
-			putchar(' ');
 		}
+		putchar('\n');
 		return (0);
 }
