0b422e09959d16d4244687fdef9606eef37d92a9
10daer <afolabireedwan@gmail.com>
2023-02-26 12:44:37 +0100

    task 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of a single digit separated by commas
+ *
+ * Return: 0
+ */
+int main(void)
+{
+		int i;
+
+		for (i = 0; i <= 89; i++)
+		{
+			if (i <= 9)
+			{
+				putchar('0' + i);
+			} else
+			{
+				putchar('0' + i / 10);
+				putchar('0' + i % 10);
+			}
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+}
